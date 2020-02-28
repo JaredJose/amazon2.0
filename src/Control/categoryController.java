6607158f51a199b1c3705b4883834a4ca82e1640
Andrei Sol <gmsoll@yahoo.com>
2020-02-28 14:09:14 -0800

    Added category control classes and fxml view

---
+++ src/Control/categoryController.java
@@ -0,0 +1,88 @@
+package Control;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+import java.awt.event.MouseEvent;
+
+public class categoryController {
+
+    @FXML
+    private TextField txtField;
+
+    @FXML
+    private Button searchBtn;
+
+    @FXML
+    private Button cartBtn;
+
+    @FXML
+    private Button freshBtn;
+
+    @FXML
+    private Button clothesBtn;
+
+    @FXML
+    private Button gamesBtn;
+
+    @FXML
+    private Button nextLeftBtn;
+
+    @FXML
+    private Button nextRightBtn;
+
+    @FXML
+    private Button productBtn1;
+
+    @FXML
+    private Button productBtn2;
+
+    @FXML
+    private Button productBtn3;
+
+    @FXML
+    private Button productBtn4;
+
+    @FXML
+    private ImageView productBtn5;
+
+    @FXML
+    void clothesOpen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void moveListLeft(ActionEvent event) {
+
+    }
+
+    @FXML
+    void moveListRight(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openCart(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openFresh(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openGames(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openProduct(MouseEvent event) {
+
+    }
+
+}
+
